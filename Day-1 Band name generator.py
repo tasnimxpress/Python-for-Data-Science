217ff002ead5688f95f45116ef16837abd7dbb52
Tasnim A <59725045+tasnimxpress@users.noreply.github.com>
2023-09-03 23:38:30 +0600

    Add files via upload

---
+++ Day-1 Band name generator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Band name generator
+# 
+# ## based on city and pet name
+# * take input from user asking city and pet name
+# * concat the user input
+# * print band name
+
+# In[2]:
+
+
+greeting = ('Welcome to the band name generator.')
+print(greeting)
+
+city = input("Where were you born? \n")
+pet = input("What is your pet name? \n")
+band_name = (city +" "+ pet)
+print('Your band name could be' +' '+band_name)
+
+
+# ### Day 1 Complete.
+
+# ### Thanks. Keep it up.
