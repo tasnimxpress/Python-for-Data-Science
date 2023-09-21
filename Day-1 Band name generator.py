d2d29cabb49559c5e8153a5eae7be43501d2b546
Tasnim A <tasu2301@gmail.com>
2023-09-21 13:18:54 +0600

    Complete

--- Day-1 Band name generator.py
+++ Day-1 Band name generator.py
@@ -1,17 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Band name generator
+# Band name generator
 # 
 # ## based on city and pet name
 # * take input from user asking city and pet name
 # * concat the user input
 # * print band name
 
-# In[2]:
-
-
-greeting = ('Welcome to the band name generator.')
+greeting = 'Welcome to the band name generator.'
 print(greeting)
 
 city = input("Where were you born? \n")
@@ -19,7 +13,3 @@ pet = input("What is your pet name? \n")
 band_name = (city +" "+ pet)
 print('Your band name could be' +' '+band_name)
 
-
-# ### Day 1 Complete.
-
-# ### Thanks. Keep it up.
--- Day-2 Tip calculator.py
+++ Day-2 Tip calculator.py
@@ -1,64 +1,31 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Tip calculator
+#  Tip calculator
 # * take input from user : 
 #     * total Bill, 
 #     * total people, 
 #     * tip percentage
 # * print how much each person should pay.
-# 
-
-# In[1]:
-
 
 print('Welcome to the tip calculator.')
-
-
-# In[2]:
-
 
 bill_1 = input('What is the total bill? ')
 bill_2 = bill_1.strip('$')
 total_bill = float(bill_2)
-
-
-# In[3]:
-
-
 number_of_people = int(input('How many people to split the bill? '))
-
-
-# In[4]:
-
-
 tip_1 = input('what percentage tip would you like to give? ')
 tip_2 = tip_1.strip('%')
 tip_percentage = float(tip_2)
 
-
-# In[9]:
-
-
 tip = (tip_percentage * total_bill)/100
 
 print(tip)
-# In[10]:
-
 
 payable = total_bill + tip
 
 print(payable)
-# In[11]:
-
 
 each_person = round((payable/number_of_people), 1)
 
 print(each_person)
-# In[12]:
-
 
 print(f'Each person should pay: ${each_person}')
 
-
-# ### Day 2 Complete.
