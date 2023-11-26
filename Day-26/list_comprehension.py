d292637fb8d9e3a43bd2f81c4122a4c5ef73d800
Tasnim A <tasu2301@gmail.com>
2023-11-26 09:38:19 +0600

    Day-26 List comprehension

--- Day-26/list_comprehension.py
+++ Day-26/list_comprehension.py
@@ -14,3 +14,15 @@ print(comprehension)
 names = ['Tasnim', 'kona', 'tupai', 'kutush', 'jannatul', 'rumpa', 'ferdous']
 cap_name = [item.upper() for item in names if len(item) > 5]
 print(cap_name)
+
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+new_squared = []
+for n in numbers:
+    new_n = n**2
+    new_squared.append(new_n)
+print(new_squared)
+
+squared_number = [item**2 for item in numbers]
+print(squared_number)
+
