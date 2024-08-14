bbf8d94db14bc126d631ed5a510b74ad29cd6f5c
Tasnim A. <tasu2301@gmail.com>
2024-08-14 10:57:30 +0600

    Create BandNameGenerator.py
    Print, input, f-string, variables

---
+++ Day-1/BandNameGenerator.py
@@ -0,0 +1,10 @@
+"Print, input, f-string, variables"
+
+Greeting = "Welcome to the 'Band Name Generator' system.\n"
+
+print(Greeting)
+
+Q1 = input("What is the name of your city?\n")
+Q2 = input("What is the name of your pet?\n")
+
+print(f"Name of your Band could be: {Q1 + Q2}")
