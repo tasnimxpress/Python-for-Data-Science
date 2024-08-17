d980677eccdba78d5fa1ffbf5559bfa51b0b8f98
Tasnim A. <tasu2301@gmail.com>
2024-08-17 23:30:47 +0600

    List, Nested List, Randomaization

--- Day-4/RockPaperScisssors.py
+++ Day-4/RockPaperScisssors.py
@@ -32,19 +32,22 @@ element = [rock, paper, scissors]
 users_input = int(input(
     "What do you choose? Type '0' for rock, '1' for paper, '2' for Scissors: "))
 
-computer_choice = random.choice(element)
-users_choice = element[users_input]
-
-print(f"{users_choice}")
-print(f"Computers choice:\n{computer_choice}")
-
-if computer_choice == users_choice:
-    print('Draw')
-elif computer_choice == element[0] and users_choice == element[2]:
-    print("computer wins")
-elif computer_choice == element[2] and users_choice == element[1]:
-    print("computer wins")
-elif computer_choice == element[1] and users_choice == element[0]:
-    print("computer wins")
+if users_input >= 3 or users_input < 0:
+    print("You type an invalid number. You loss.")
 else:
-    print("you win")
+    computer_choice = random.choice(element)
+    users_choice = element[users_input]
+
+    print(f"{users_choice}")
+    print(f"Computers choice:\n{computer_choice}")
+
+    if computer_choice == users_choice:
+        print('Draw')
+    elif computer_choice == element[0] and users_choice == element[2]:
+        print("computer wins")
+    elif computer_choice == element[2] and users_choice == element[1]:
+        print("computer wins")
+    elif computer_choice == element[1] and users_choice == element[0]:
+        print("computer wins")
+    else:
+        print("you win")
