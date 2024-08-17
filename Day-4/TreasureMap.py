049288f1846330abba8e81f87bb5103a093582ed
Tasnim A. <tasu2301@gmail.com>
2024-08-17 21:33:52 +0600

    List, Nested List, Randomaization

--- Day-4/TreasureMap.py
+++ Day-4/TreasureMap.py
@@ -4,21 +4,33 @@ row1 = ['▥', '▥', '▥']
 row2 = ['▥', '▥', '▥']
 row3 = ['▥', '▥', '▥']
 
-row = [row1, row2, row3]
+column = ['A', 'B', 'C']
+map = [row1, row2, row3]
 
-treasure_vault = input('Where do you want to keep your treasure? ')
+treasure_vault = input('Where do you want to keep your treasure? ').upper()
 
-column = int(treasure_vault[-1])
+column_index = treasure_vault[0]
+
+column_position = column.index(column_index)
+row_position = int(treasure_vault[-1])
+
+map[column_position][row_position-1] = 'X'
+
+
+print(f'{row1}\n{row2}\n{row3}')
+
+
+"Alternative approach"
 
 if treasure_vault[0] == 'A':
-    row1[column-1] = "X"
+    row1[row_position-1] = "X"
 elif treasure_vault[0] == 'B':
-    row2[column-1] = "X"
+    row2[row_position-1] = "X"
 else:
-    row3[column-1] = "X"
+    row3[row_position-1] = "X"
 
 
-if treasure_vault in ('A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'):
+if treasure_vault in ("A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"):
     print(f'Hiding your treasure, X marks the spot.\n{row1}\n{row2}\n{row3}')
 else:
     print('Enter a valid vault number. Available vault: \nA1, A2, A3, B1, B2, B3, C1, C2, C3.')
