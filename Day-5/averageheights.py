aab63e59bb5a56f5bd4bb99c10a5e2b1d81ca4ce
Tasnim A. <tasu2301@gmail.com>
2024-08-18 13:21:22 +0600

    List, For loop

--- Day-5/averageheights.py
+++ Day-5/averageheights.py
@@ -5,11 +5,17 @@ heights = input('Enter heights: ').split()
 for i in range(0, len(heights)):
     heights[i] = int(heights[i])
 
-average_heights = sum(heights)/len(heights)
+average_heights = round(sum(heights)/len(heights), 2)
+num_people = len(heights)
+total_height = sum(heights)
+
+print(f'Total height: {total_height}')
+print(f'Number of people: {num_people}')
 print(f'Average heights: {average_heights}')
 
 
 "Alternative approach"
+print('\nResult from alternative approach.\n')
 
 total_sum = 0
 for i in heights:
@@ -17,5 +23,8 @@ for i in heights:
 
 # print(total_sum)
 
-average = total_sum/len(heights)
+average = round(total_sum/len(heights), 2)
+
+print(f'Total height: {total_sum}')
+print(f'Number of people: {len(heights)}')
 print(f'Average height: {average}')
