d8ad5357e3fa7e92b4ab10704c4ffecbc9bff398
Tasnim A. <tasu2301@gmail.com>
2024-08-18 14:57:55 +0600

    List, for loop, if-else

--- Day-5/AddEvenNumber.py
+++ Day-5/AddEvenNumber.py
@@ -8,7 +8,15 @@ if Range > 1000:
 else:
     for n in range(1, Range + 1):
         if n % 2 == 0:
-            print(n)
             total += n
 
     print(total)
+
+
+# OR
+
+sum_total = 0
+for i in range(0, Range + 1, 2):
+    sum_total += i
+
+print(f"Another Approach result: {sum_total}")
