b62a7881f6cb5c98109f186a9f871e4227d96d4e
Tasnim A. <tasu2301@gmail.com>
2024-08-19 21:09:57 +0600

    Functions

--- Day-6/Functions.py
+++ Day-6/Functions.py
@@ -12,3 +12,24 @@ def print_hello():
 
 
 print_hello()
+
+# Reeborgs world
+
+# def turn_around():
+#     turn_left()
+#     turn_left()
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# # Draw square
+# turn_left()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
