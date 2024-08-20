f3f7c9db6cee28b38763ea1ced987ad3390bae32
Tasnim A. <tasu2301@gmail.com>
2024-08-20 11:37:30 +0600

    For & while loop, if-else, list, string, range

---
+++ Day-7/Hangman.py
@@ -0,0 +1,5 @@
+"""
+Hangman is a guessing game for two or more players. 
+One player thinks of a word, phrase, or sentence and the other(s) tries to guess it 
+by suggesting letters or numbers within a certain number of guesses. 
+Originally a paper-and-pencil game, this is a virtual version of the game where you will play with computer generated word."""
