1d793e292d182cda8234a2a1c0e23385d4d766de
Tasnim A. <tasu2301@gmail.com>
2024-08-20 11:15:56 +0600

    Functions, While loop

--- Day-6/Functions.py
+++ Day-6/Functions.py
@@ -13,50 +13,6 @@ def print_hello():
 
 print_hello()
 
-# Reeborgs world
-
-# def turn_around():
-#     turn_left()
-#     turn_left()
-
-# def turn_right():
-#     turn_left()
-#     turn_left()
-#     turn_left()
-
-# # Draw square
-# turn_left()
-# move()
-# turn_right()
-# move()
-# turn_right()
-# move()
-# turn_right()
-# move()
-
-
-# def jump():
-#     move()
-#     turn_left()
-#     move()
-#     turn_right()
-#     move()
-#     turn_right()
-#     move()
-#     turn_left()
-
-
-# jump()
-
-
-# function 2
-
-# def myfunction():
-#     if sky == 'clear':
-#         print('blue')
-#     else:
-#         print('hello')
-
 
 # While loop
 
@@ -65,16 +21,7 @@ print_hello()
 # Do this
 
 
-# number = 10
-# while number > 10:
-#     print(number)
-#     number -= 1
-
-# number_of_hurdle = 6
-# while number_of_hurdle > 0:
-# jump()
-# number_of_hurdle -= 1
-
-# at_goal() == True
-# while not at_goal():
-#     jump()
+number = 0
+while number < 10:
+    print('hello..this is true.')
+    number += 1
