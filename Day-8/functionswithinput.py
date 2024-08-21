bb4736d24c952654168b3c374b7d30e8404e1404
Tasnim A. <tasu2301@gmail.com>
2024-08-21 11:39:11 +0600

    Functions with input, arguments, parameters

--- Day-8/functionswithinput.py
+++ Day-8/functionswithinput.py
@@ -18,3 +18,16 @@ def greet_with_name(name):
 
 
 greet_with_name('tasnim')
+
+# functions with more than 1 input
+
+
+def greet_with(name, location):
+    print(f'How are you {name}?')
+    print(f'How much do you know about {location}?')
+
+
+# Positional argumrnt
+greet_with('tasnim', 'dhaka')
+# Keyword argument
+greet_with(location='Dhaka', name='Tasnim')
