ecd4c1b3af7c161eeebde57cac4e0daedc91623e
Tasnim A. <tasu2301@gmail.com>
2024-08-21 12:30:29 +0600

    Functions with input, arguments, parameters

--- Day-8/functionswithinput.py
+++ Day-8/functionswithinput.py
@@ -1,51 +1,68 @@
 import math
 "Functions with input, arguments, parameters"
 
-# function
+# # function
 
 
-from math import ceil, floor
+# from math import ceil, floor
 
 
-def greet():
-    for n in range(3):
-        print('Hello')
+# def greet():
+#     for n in range(3):
+#         print('Hello')
 
 
-greet()
+# greet()
 
-# function with input
-
-
-def greet_with_name(name):
-    print(f'How are you, {name}?')
+# # function with input
 
 
-greet_with_name('tasnim')
-
-# functions with more than 1 input
-
-
-def greet_with(name, location):
-    print(f'How are you {name}?')
-    print(f'How much do you know about {location}?')
+# def greet_with_name(name):
+#     print(f'How are you, {name}?')
 
 
-# Positional argumrnt
-greet_with('tasnim', 'dhaka')
-# Keyword argument
-greet_with(location='Dhaka', name='Tasnim')
+# greet_with_name('tasnim')
+
+# # functions with more than 1 input
 
 
-# function in practice
+# def greet_with(name, location):
+#     print(f'How are you {name}?')
+#     print(f'How much do you know about {location}?')
 
 
-def paint_calc(height, width, cover):
-    total_can = ceil((height * width) / cover)
-    print(f'You need {total_can} cans of paint.')
+# # Positional argumrnt
+# greet_with('tasnim', 'dhaka')
+# # Keyword argument
+# greet_with(location='Dhaka', name='Tasnim')
 
 
-test_h = int(input('Enter height: '))
-test_w = int(input('Enter widht: '))
-coverage = 5
-paint_calc(test_h, test_w, coverage)
+# # function in practice
+
+
+# def paint_calc(height, width, cover):
+#     total_can = ceil((height * width) / cover)
+#     print(f'You need {total_can} cans of paint.')
+
+
+# test_h = int(input('Enter height: '))
+# test_w = int(input('Enter widht: '))
+# coverage = 5
+# paint_calc(test_h, test_w, coverage)
+
+
+# Is Prime number
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+
+    if is_prime:
+        print(f'{number} is prime')
+    else:
+        print(f'{number} is not prime')
+
+
+num = int(input('Enter number: '))
+prime_checker(num)
