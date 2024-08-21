746481e1229d002cb2794c6daac2f85632558869
Tasnim A. <tasu2301@gmail.com>
2024-08-21 13:52:54 +0600

    Functions with input, arguments, parameters

---
+++ Day-8/Ceasercipher.py
@@ -0,0 +1,23 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+# Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+
+def encrypt(messege, key):
+    plain_text = []
+    for i in messege:
+        position = alphabet.index(i) + key
+        char = alphabet[position]
+        plain_text.append(char)
+
+    print(f"The encoded text is: {"".join(plain_text)}")
+
+
+encrypt(text, shift)
