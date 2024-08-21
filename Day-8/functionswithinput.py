ccd2815d5a0351ebc2a0ae4d34860ca27ef331b8
Tasnim A. <tasu2301@gmail.com>
2024-08-21 11:28:14 +0600

    Functions with input, arguments, parameters

--- Day-8/functionswithinput.py
+++ Day-8/functionswithinput.py
@@ -1,1 +1,20 @@
 "Functions with input, arguments, parameters"
+
+# function
+
+
+def greet():
+    for n in range(3):
+        print('Hello')
+
+
+greet()
+
+# function with input
+
+
+def greet_with_name(name):
+    print(f'How are you, {name}?')
+
+
+greet_with_name('tasnim')
