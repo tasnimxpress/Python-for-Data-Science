bb3dbf0e50c9301789a9051ca437f54a934dac68
Tasnim A. <tasu2301@gmail.com>
2024-08-22 11:32:42 +0600

    Dictionary

--- Day-9/Dictionary.py
+++ Day-9/Dictionary.py
@@ -1,1 +1,13 @@
 # Dictionary
+
+# creating dictionary
+programming_dictionary = {
+    'Bug': 'Error in a code that prevent code to run as expected.',
+    'Loop': 'A loop is a sequence of instruction s that is continually repeated until a certain condition is reached.',
+
+}
+print(programming_dictionary)
+
+# add key: value
+programming_dictionary['Recursion'] = 'Recursion is a programming concept in which a function, class method, or routine invokes itself to re-apply an algorithm on a different set of inputs.'
+print(programming_dictionary)
