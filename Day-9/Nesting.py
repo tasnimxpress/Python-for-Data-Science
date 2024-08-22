a546825d50d2eeef1ccb1818391ae23ba03bab96
Tasnim A. <tasu2301@gmail.com>
2024-08-22 12:30:48 +0600

    Dictionary, Nested Dictionary

---
+++ Day-9/Nesting.py
@@ -0,0 +1,15 @@
+"Nesting dictionary"
+
+capitals = {
+    'Bangladesh': 'Dhaka',
+    'Germany': 'Berlin',
+    'Australia': 'Sydney'
+}
+
+# nesting dictionary in a dictionary
+travel_log = {
+    'Bandarban': {'cities_visited': ['golden temple', 'Boga lake', 'Nilgiri hill', 'Chimbuk hill'], 'total_visit': 2},
+    'Coxbazar': {'cities_visited': ['Inani', 'Kolatoli', 'Alikodom'], 'total_visit': 1}
+}
+
+print(travel_log)
