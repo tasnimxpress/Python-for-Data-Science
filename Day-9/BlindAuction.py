e99ca899b3a37cdbfdfeb6875f2bfdf4e88c7a4d
Tasnim A. <tasu2301@gmail.com>
2024-08-22 16:34:06 +0600

    Dictionary, Nested Dictionary

--- Day-9/BlindAuction.py
+++ Day-9/BlindAuction.py
@@ -4,10 +4,14 @@ In this type of auction, all bidders simultaneously submit sealed bids so that n
 The highest bidder pays the price that was submitted."""
 
 import os
+import time
+from auctionlogo import logo, Winner_logo
+
 bidders = {}
 
 auction_continue = True
 while auction_continue:
+    print(f'{logo}\nWelcome to the Blind Auction.\n')
     name = input('Enter your name: ')
     bid = int(input('Enter your bid: $'))
 
@@ -20,16 +24,23 @@ while auction_continue:
 
     if users == 'n':
         auction_continue = False
-        print('We are closing the auction.\n')
+        print('Bidding is closed.\nWe are analyzing the bids.')
 
+        time.sleep(3)
         # print(bidders)
 
-highest_bidder = 0
-winner = {}
-for key in bidders:
-    if bidders[key] > highest_bidder:
-        highest_bidder = bidders[key]
-        winner['Name'] = key
-        winner['Bid'] = bidders[key]
 
-print(f'The winner is {winner["Name"]} with a bid of ${winner['Bid']}.')
+def result(bidding_record):
+    highest_bidder = 0
+    winner = {}
+    for key in bidding_record:
+        if bidding_record[key] > highest_bidder:
+            highest_bidder = bidding_record[key]
+            winner['Name'] = key
+            winner['Bid'] = bidding_record[key]
+
+    print(f'\nWe got the result.\n{Winner_logo}:\nThe winner is {
+        winner["Name"]} with a bid of ${winner['Bid']}.')
+
+
+result(bidders)
