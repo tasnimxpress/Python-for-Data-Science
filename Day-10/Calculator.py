2a1fb305c5ef7dba67f1d608d0828a26beb96d07
Tasnim A. <tasu2301@gmail.com>
2024-08-24 14:17:17 +0600

    )

--- Day-10/Calculator.py
+++ Day-10/Calculator.py
@@ -25,3 +25,13 @@ symbols = {
     '*': multiply,
     '/': divide
 }
+
+n1 = float(input('First number: '))
+for key in symbols:
+    print(key)
+operation = input(f'Which operation you want to do: ')
+n2 = float(input('second number: '))
+
+
+calculation = symbols[operation]
+print(calculation(n1, n2))
