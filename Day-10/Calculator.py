0aa1ca068dc7d81e60d38c7e75de1b65fd5118be
Tasnim A. <tasu2301@gmail.com>
2024-08-24 14:22:00 +0600

    Functions with output

--- Day-10/Calculator.py
+++ Day-10/Calculator.py
@@ -26,12 +26,13 @@ symbols = {
     '/': divide
 }
 
-n1 = float(input('First number: '))
+n1 = float(input('Enter first number: '))
 for key in symbols:
     print(key)
-operation = input(f'Which operation you want to do: ')
-n2 = float(input('second number: '))
+operation = input(f'Pick an operation from above: ')
+n2 = float(input('Enter second number: '))
 
 
 calculation = symbols[operation]
-print(calculation(n1, n2))
+answer = calculation(n1, n2)
+print(f'{n1} {operation} {n2} = {answer}')
