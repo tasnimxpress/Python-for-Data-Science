f230ef8f7fd46ab4bc1ce370d4ed37e170fb1b38
Tasnim A. <tasu2301@gmail.com>
2024-08-24 17:20:08 +0600

    Functions with output, Recursion

--- Day-10/Recursion.py
+++ Day-10/Recursion.py
@@ -17,3 +17,9 @@ def fib(num):
     if num == 0 or num == 1:
         return 1
     return fib(num - 1) + fib(num - 2)
+
+
+nth_number = int(input('Enter nth number: '))
+
+for i in range(nth_number):
+    print(fib(i))
