7874ba6c1aaebcf6a65a21835db1dcc51b3859f0
Tasnim A. <tasu2301@gmail.com>
2024-08-27 13:57:50 +0600

    Capstone project - Higher Lower game

---
+++ Day-14/main.py
@@ -0,0 +1,6 @@
+# Capstone project - Higher Lower game
+
+"""
+The objective is simple: guess which of the two cards shown is higher. 
+If you get the answer correct, a new card will appear and simply guess again which you think is higher. 
+Every game has infinite questions so try your best to get a high score streak."""
