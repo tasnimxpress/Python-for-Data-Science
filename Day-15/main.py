a017199682d97c501f1094b18f48b5bcbb0b23e9
Tasnim A. <tasu2301@gmail.com>
2024-08-28 11:49:17 +0600

    Capstone project - Coffee machine

--- Day-15/main.py
+++ Day-15/main.py
@@ -1,3 +1,13 @@
 # Capstone project - Coffee machine
 
 from resources import MENU, CAPACITY
+
+order = input('What do you like? (espresso/latte/cappuccino): ').lower()
+
+if order == 'report':
+    for item in CAPACITY:
+        # print(item)
+        print(f"{item}: {CAPACITY[item]}")
+
+# for order in MENU:
+print(MENU[order]['ingredients'])
