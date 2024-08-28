9002bfcc96cf8126ec237167310f930a5913a282
Tasnim A. <tasu2301@gmail.com>
2024-08-28 15:01:08 +0600

    Capstone project - Coffee machine

--- Day-15/main.py
+++ Day-15/main.py
@@ -1,13 +1,58 @@
 # Capstone project - Coffee machine
 
-from resources import MENU, CAPACITY
+from resources import MENU, RESOURSE
+
+
+def check_resourse(customer_order, machine_capacity):
+    item_needed = MENU[customer_order]['ingredients']
+    item_cost = MENU[customer_order]['cost']
+
+    current_resourse = {}
+    for i in RESOURSE:
+        try:
+            current_resourse[i] = machine_capacity[i] - item_needed[i]
+            current_resourse['money'] = item_cost
+        except:
+            current_resourse[i] = machine_capacity[i]
+
+    return (current_resourse)
+
+
+def report(data):
+    initial_report = {}
+
+    initial_report['water'] = data['water']
+    initial_report['milk'] = data['milk']
+    initial_report['coffee'] = data['coffee']
+    initial_report['money'] = 0
+
+    return initial_report
+
+
+def format(report_data):
+    Water = report_data['water']
+    Milk = report_data['milk']
+    Coffee = report_data['coffee']
+    Money = report_data["money"]
+
+    return (f'Water: {Water} ml\nMilk: {
+        Milk} ml\nCoffee: {Coffee} g\nMoney: ${Money}')
+
 
 order = input('What do you like? (espresso/latte/cappuccino): ').lower()
+start_report = report(RESOURSE)
+print(format(start_report))
 
-if order == 'report':
-    for item in CAPACITY:
-        # print(item)
-        print(f"{item}: {CAPACITY[item]}")
+machine_on = True
+while machine_on:
+    user = order
+    print(f'heres your {user}')
+    order = input('What do you like? (espresso/latte/cappuccino): ').lower()
+    resourses = check_resourse(user, RESOURSE)
 
-# for order in MENU:
-print(MENU[order]['ingredients'])
+    if order == 'report':
+        resourses = check_resourse(user, RESOURSE)
+        print(format(resourses))
+
+    if order == 'off':
+        machine_on = False
--- Day-15/resources.py
+++ Day-15/resources.py
@@ -24,7 +24,7 @@ MENU = {
     }
 }
 
-CAPACITY = {
+RESOURSE = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
