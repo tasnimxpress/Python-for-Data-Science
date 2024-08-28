03ffc33479a0058671c48f31a1fd3c4088c1f8e7
Tasnim A. <tasu2301@gmail.com>
2024-08-28 21:35:14 +0600

    Capstone project - Coffee machine

--- Day-15/main.py
+++ Day-15/main.py
@@ -1,19 +1,33 @@
 # Capstone project - Coffee machine
 
 from resources import MENU, RESOURSE
-
+import os
+import time
 PROFIT = 0
 
 
-def report(data):
+def report(inventory):
+    """
+    Generates a report of the machine's current stock and profit information.
+
+    Args:
+        inventory (dict): A dictionary representing the current stock of resources in the machine, 
+        where keys are ingredient names and values are their available quantities.
+    """
     initial_report = {}
 
-    initial_report['ingredients'] = data
+    initial_report['ingredients'] = inventory
     initial_report['cost'] = PROFIT
     return initial_report
 
 
 def format(report_data):
+    """
+    Formats the machine's stock and profit data into a readable string.
+
+    Returns:
+        str: A formatted string displaying the quantities of items in machine's stock, as well as the total profit in dollars.
+    """
     Water = report_data['ingredients']['water']
     Milk = report_data['ingredients']['milk']
     Coffee = report_data['ingredients']['coffee']
@@ -23,19 +37,36 @@ def format(report_data):
         Milk} ml\nCoffee: {Coffee} g\nMoney: ${Money}')
 
 
-def remaining_resourse(customer_order, machine_capacity):
+def remaining_resourse(customer_order, inventory):
+    """
+    Calculates the remaining resources in the machine's stock after fulfilling a customer's order.
+
+    Returns:
+        dict: A dictionary showing the remaining quantities of each resource in the machine's stock after subtracting the quantities required for the customer's order. If an ingredient is not needed for the order, its original value from the inventory is retained.
+    """
+
     current_resourse = {}
-    for i in set(machine_capacity) | set(customer_order):
+    for i in set(inventory) | set(customer_order):
         try:
-            remaining_resourse = machine_capacity[i] - customer_order[i]
+            remaining_resourse = inventory[i] - customer_order[i]
         except:
-            remaining_resourse = machine_capacity.get(i)
+            remaining_resourse = inventory.get(i)
         current_resourse[i] = remaining_resourse
 
     return current_resourse
 
 
 def is_resourse_sufficient(order, remaining_resourse):
+    """
+    Checks if available resources are sufficient for a customer's order.
+
+    Args:
+        order (dict): Required ingredients and their quantities.
+        remaining_resourse (dict): Available ingredients and their quantities.
+
+    Returns:
+        bool: True if all ingredients are sufficient; otherwise False, with a message indicating any shortage.
+    """
     for item in order:
         if order[item] > remaining_resourse[item]:
             print(f"We dont have enough {item}")
@@ -43,22 +74,59 @@ def is_resourse_sufficient(order, remaining_resourse):
     return True
 
 
+def check_transaction(item_price, inserted_amount):
+    """
+    Checks if the inserted amount is sufficient for the item price and provides the necessary feedback.
+
+    Args:
+        item_price (float): The price of the item being purchased.
+        inserted_amount (float): The amount of money inserted by the customer.
+
+    Returns:
+        bool: True if the inserted amount is equal to or greater than the item price; False otherwise. Prints the amount of change if applicable, or a refund message if insufficient money.
+    """
+    if item_price < inserted_amount:
+        change = round(inserted_amount - item_price, 2)
+        print(f"Here is {change} dollars in change.")
+        return True
+    elif item_price == inserted_amount:
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+
 machine_on = True
 while machine_on:
     order = input('What do you like? (espresso/latte/cappuccino): ').lower()
 
-    # START_REPORT = report(RESOURSE)
-
     if order == 'report':
         print(format(report(RESOURSE)))
+    elif order == 'clean':
+        os.system('cls')
     elif order == 'off':
+        os.system('cls')
         machine_on = False
     else:
         choice = MENU[order]['ingredients']
 
         check_resourse = is_resourse_sufficient(choice, RESOURSE)
         if check_resourse is True:
-            print(f"Here is your {order}. Enjoy!")
-            PROFIT += MENU[order]['cost']
-            current_resourse = remaining_resourse(choice, RESOURSE)
-            RESOURSE = current_resourse
+            print('Please insert coins.')
+            quarter = float(input('How many quarters: '))
+            nickel = float(input('How many nickels: '))
+            dime = float(input('How many dimes: '))
+            penny = float(input('How many pennies: '))
+
+            total_coins = (quarter * 0.25) + (nickel * 0.05) + \
+                (dime * 0.10) + (penny * 0.01)
+            price = MENU[order]['cost']
+
+            transaction = check_transaction(price, total_coins)
+            if transaction is True:
+                print(f"\nHere is your {order}. Enjoy!")
+                PROFIT += MENU[order]['cost']
+                current_resourse = remaining_resourse(choice, RESOURSE)
+                RESOURSE = current_resourse
+                time.sleep(5)
+                os.system('cls')
