8a3cabc466f60dd4dc3ed9b048fa4b2b556eefd2
Tasnim A. <tasu2301@gmail.com>
2024-08-29 16:41:06 +0600

    OOP

--- Day-16/main.py
+++ Day-16/main.py
@@ -3,24 +3,22 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 coffee_machine = CoffeeMaker()
-transaction = MoneyMachine()
-
+money_machine = MoneyMachine()
 menu = Menu()
 
-
-profit = 0
 machine_on = True
+
 while machine_on:
     item = menu.get_items()
     order = input(f'What would you like? ({item}): ')
 
     if order == 'report':
         coffee_machine.report()
+        money_machine.report()
     elif order == 'off':
         machine_on = False
     else:
         drink = menu.find_drink(order)
-        coffee_machine.is_resource_sufficient(drink)
-        money = transaction.make_payment(drink)
-        if money is True:
-            coffee_machine.make_coffee(drink)
+        if coffee_machine.is_resource_sufficient(drink) is True:
+            if money_machine.make_payment(drink.cost) is True:
+                coffee_machine.make_coffee(drink)
