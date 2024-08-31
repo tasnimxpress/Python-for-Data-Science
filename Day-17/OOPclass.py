0a58eb04a1055d8cfb1614926df9f6c7fb03411f
Tasnim A. <tasu2301@gmail.com>
2024-08-31 12:08:56 +0600

    OOP, class

--- Day-17/OOPclass.py
+++ Day-17/OOPclass.py
@@ -1,12 +1,19 @@
 # creating class
 class User:
     # initialize attributes
+    # attributes that the object has - like characteristics
     def __init__(self, userid, username):
         self.id = userid
         self.name = username
         self.follower = 0  # set default value
+        self.following = 0
 
-    pass
+    # creating method
+    # method that a object does - how a object act
+    # method is functions associated with object
+    def follow(self, user):
+        user.follower += 1
+        self.following += 1
 
 
 # creating object from class
@@ -18,4 +25,8 @@ Object_2 = User(2, 'user_2')
 
 print(Object_1.id)
 print(Object_2.name)
+
+Object_1.follow(Object_2)
+
+print(Object_2.follower)
 print(Object_1.follower)
