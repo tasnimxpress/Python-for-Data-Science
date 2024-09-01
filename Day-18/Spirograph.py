9a723d23210c5e9ab2e25c8945d7b3762150c7a0
Tasnim A. <tasu2301@gmail.com>
2024-09-01 15:46:46 +0600

    OOP - Turtle graphics, tuple

--- Day-18/Spirograph.py
+++ Day-18/Spirograph.py
@@ -1,1 +1,23 @@
 # create a spirograph using turtle module
+
+from turtle import Turtle as t, Screen, colormode
+from random import randrange
+
+screen = Screen()
+graph = t()
+colormode(255)
+graph.speed(0)
+
+circle_continue = True
+while circle_continue:
+
+    random_color = (randrange(255), randrange(255), randrange(255))
+    graph.color(random_color)
+    graph.circle(100)
+    graph.setheading(graph.heading() + 10)
+
+    if graph.heading() == 0.0:
+        circle_continue = False
+
+
+screen.exitonclick()
