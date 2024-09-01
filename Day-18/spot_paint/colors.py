cc78ee27373e6af12aa3c78009f025ca1537a1ef
Tasnim A. <tasu2301@gmail.com>
2024-09-01 16:42:44 +0600

    OOP - Turtle graphics, Tuples

--- Day-18/spot_paint/colors.py
+++ Day-18/spot_paint/colors.py
@@ -1,16 +1,14 @@
 import colorgram as cg
 
-image = cg.extract('Day-18/hirst_spot_paint.png', 7)
+image = cg.extract('Day-18/spot_paint/hirst_spot_paint.png', 7)
 
 color_list = []
-for i in range(len(image)):
-    color = image[i]
-    rgb_color = color.rgb
-    r = rgb_color.r
-    g = rgb_color.g
-    b = rgb_color.b
+for color in image:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
     rgb = (r, g, b)
 
     color_list.append(rgb)
 
-print(color_list)
+# print(color_list)
---
+++ Day-18/spot_paint/main.py
@@ -0,0 +1,3 @@
+from colors import color_list
+
+# print(color_list)
