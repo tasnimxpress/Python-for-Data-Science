a23ba5aaabe60c152002026547d8c7857ddf6ae7
Tasnim A. <tasu2301@gmail.com>
2024-09-06 19:56:17 +0600

    inheritance

--- Day-22/multiple_inheritance.py
+++ Day-22/multiple_inheritance.py
@@ -1,1 +1,28 @@
-#
+# multiple inheritance
+# first 2 class will be the base class for 3rd class
+
+
+class OperatingSystem:
+    def __init__(self):
+        self.multitasking = True
+        self.name = 'Unix OS'
+
+
+class Apple:
+    def __init__(self):
+        self.Website = 'www.apple.com'
+        self.name = 'Mac OS'
+
+
+class Macbook(OperatingSystem, Apple):
+    def __init__(self):
+        OperatingSystem.__init__(self)
+        Apple.__init__(self)
+
+        if self.multitasking is True:
+            print(f'This is a multitasking system based on {self.name}. Visit {
+                  self.Website} to know more.')
+
+
+# if both class has same attribute name, 2nd class will overwrite the first class attribut in output
+macbook = Macbook()
