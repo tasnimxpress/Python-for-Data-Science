6e01ae71865cefa5fee7c667ead569f4e2351906
Tasnim A. <tasu2301@gmail.com>
2024-09-09 19:27:34 +0600

    Inheritance, Naming convention, polymorphism

--- Day-22/OOP-overloading.py
+++ Day-22/OOP-overloading.py
@@ -5,6 +5,12 @@ class Square:
     def __init__(self, lengthOfSide):
         self.side = lengthOfSide
 
+    def __add__(n1, n2):
+        return ((n1.side * 4) + (n2.side * 4))
+
 
 square1 = Square(5)
 square2 = Square(10)
+
+
+print(f'sum {square1 + square2}')
