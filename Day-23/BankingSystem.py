b15b64ce28ca8599855783c713a25979671e998d
Tasnim A. <tasu2301@gmail.com>
2024-09-10 19:33:48 +0600

    Capstone project - OOP - Banking System

--- Day-23/BankingSystem.py
+++ Day-23/BankingSystem.py
@@ -13,3 +13,37 @@
             * deposit or 
             * display their available balance.
 """
+
+
+class Bank:
+    def __init__(self):
+        self.initialBalance = 0
+
+    def createAccount(self, name, deposit):
+        self.initialBalance += deposit
+        with open('E:/GitHub/100DaysOfPython/Day-23/AccountDetails.csv', 'w') as acc:
+            details = acc.write(f"'name': {name}, 'balance': {
+                                self.initialBalance}")
+            return details
+
+    def displayDetails(self):
+        # self.createAccount(name='name', deposit=self.initialBalance)
+        with open('E:/GitHub/100DaysOfPython/Day-23/AccountDetails.csv', 'r') as acc:
+            print(acc.read())
+
+
+class customer:
+    pass
+
+
+bank = Bank()
+# user menu
+print('Type 1 to create a new account.\nType 2 to log in an existing account: ')
+choice = int(input())
+
+if choice == 1:
+    user_name = input('Enter your name: ')
+    deposit = int(input('Enter your deposit amount: '))
+    bank.createAccount(user_name, deposit)
+elif choice == 2:
+    bank.displayDetails()
