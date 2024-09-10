0df3e14a3ef15c608cd98a13d0517e86ee44c5b1
Tasnim A. <tasu2301@gmail.com>
2024-09-10 17:06:26 +0600

    open, read, write Files

---
+++ Day-23/BankingSystem.py
@@ -0,0 +1,15 @@
+"""
+1. Give a prompt to the user asking if:
+    * they wish to create a new savings account 
+    * or access an existing one.
+2. if the user would like to create a new account, 
+    * then accept their name and initial deposit, 
+    * and create a 5 digit random number 
+    * and make it as the account number of their new savings account.
+3. if they are accessing an existing account:
+    * accept their name and account number to validate the user
+    * give them options to: 
+            * withdraw, 
+            * deposit or 
+            * display their available balance.
+"""
---
+++ Day-24/workingwithfile.py
@@ -0,0 +1,12 @@
+# read the file mode='r'
+with open('E:/GitHub/100DaysOfPython/Day-24/file.txt', 'r') as file:
+    print(file.read())
+
+
+# overwrite the file mode='w'
+with open('E:/GitHub/100DaysOfPython/Day-24/file.txt', 'w') as file:
+    file.write('\nthis is new text')
+
+# write to the file without deleting the previous text : mode='a'
+with open('E:/GitHub/100DaysOfPython/Day-24/file.txt', 'a') as file:
+    file.write('\nThis is text without deleting first text')
