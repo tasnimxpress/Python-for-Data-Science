d53ff1180d740baac653bf6ce953de7044e63a23
Tasnim A. <tasu2301@gmail.com>
2024-09-14 10:21:15 +0600

    Capstone project - OOP - Banking System

--- Day-23/BankingSystem.py
+++ Day-23/BankingSystem.py
@@ -16,7 +16,6 @@
 
 from abc import ABCMeta, abstractmethod
 import random
-import csv
 import pandas as pd
 PATH = "E:/GitHub/100DaysOfPython/Day-23/AccountDetails.csv"
 
@@ -43,47 +42,75 @@ class Account(metaclass=ABCMeta):
         return 0
 
 
-class Bank:
+class SavingsAccount(Account):
     def __init__(self):
-        self.initialBalance = 0
-        self.details = {}
-        self.data = pd.DataFrame(self.details)
-        self.csv = self.data.to_csv(PATH)
+        self.AccountDetails = {}
 
     def createAccount(self, name, deposit):
-        pin = random.randint(10000, 99999)
-        self.details[pin] = [name, deposit]
+        self.accountNumber = random.randint(10000, 99999)
+        self.AccountDetails[self.accountNumber] = [name, deposit]
+        print(f'\nAccount has been created, account number: {
+              self.accountNumber}\n')
 
-    def authenticate(name, accountNumber):
-        pass
+    def authenticate(self, name, accountNumber):
+        if accountNumber in self.AccountDetails.keys():
+            if self.AccountDetails[accountNumber][0] == name:
+                print('\nAuthentication successfull.\n')
+                self.accountNumber = accountNumber
+                return True
+        else:
+            print('\nAuthentication Failed.\n')
+            return False
 
-    def withdraw(self, amount):
-        pass
+    def withdraw(self, withdrawalAmount):
+        if withdrawalAmount > self.AccountDetails[self.accountNumber][1]:
+            print('\nInsufficient Balance.')
+        else:
+            self.AccountDetails[self.accountNumber][1] -= withdrawalAmount
+            print(f'\nCashout successfull.')
+            self.displayBalance()
 
-    def deposti(self, amount):
-        pass
+    def deposit(self, depositAmount):
+        self.AccountDetails[self.accountNumber][1] += depositAmount
+        print(f'\nDeposit successfull.')
+        self.displayBalance()
 
     def displayBalance(self):
-        pass
+        print(f'\nAvailable balance: {
+              self.AccountDetails[self.accountNumber][1]}')
 
 
-class customer:
-    pass
-
-
-bank = Bank()
-print(bank.details)
+account = SavingsAccount()
 
 
 # user menu
-bankOpen = True
-while bankOpen:
-    print('Type 1 to create a new account.\nType 2 to log in an existing account: ')
+while True:
+    print('Type 1 to create a new account.\nType 2 to log in an existing account\nType 3 to close the app: ')
     choice = int(input())
 
     if choice == 1:
-        user_name = input('Enter your name: ')
+        user_name = input('\nEnter your name: ')
         deposit = int(input('Enter your deposit amount: '))
-        bank.createAccount(user_name, deposit)
+        account.createAccount(user_name, deposit)
     elif choice == 2:
-        print(bank.displayDetails())
+        name = input('\nEnter your user name: ')
+        pin = int(input('Enter your pin: '))
+        authentication = account.authenticate(name, pin)
+
+        if authentication is True:
+            while True:
+                print(
+                    '\nType 1 to deposit.\nType 2 to withdraw.\nType 3 to see available balance.\nType 4 to log out: ')
+                user_choice = int(input())
+                if user_choice == 1:
+                    depositAmount = int(input('\nEnter deposit amount: '))
+                    account.deposit(depositAmount)
+                elif user_choice == 2:
+                    cashoutAmount = int(input('\nEnter withdrawal amount: '))
+                    account.withdraw(cashoutAmount)
+                elif user_choice == 3:
+                    account.displayBalance()
+                elif user_choice == 4:
+                    break
+    elif choice == 3:
+        quit()
