629bc92d3c49248e5f0b1c7267485cbb43f4c5b2
Tasnim A. <tasu2301@gmail.com>
2024-10-05 16:46:09 +0600

    Pandas

--- Day-25/squirrel_data.py
+++ Day-25/squirrel_data.py
@@ -3,12 +3,13 @@ import pandas as pd
 df = pd.read_csv('Squirrel_Census.csv')
 
 color = df.groupby(['Primary Fur Color']).count()
+# print(color)
 # print(type(color)) # dataframe
 
 color_series = color['X']
-print(color_series)
+# print(color_series)
+# print(type(color_series)) # Series
 
-# print(type(color_series))
-new_df = pd.DataFrame(
-    ['color_series', index = 'Primary Fur Color', columns = 'X']).squeeze()
+new_df = pd.DataFrame({'count': color_series})
 print(new_df)
+new_df.to_csv('numberOfSquirell.csv')
