d9514ea27a08e86970013941c3b7f5f745e422ab
Tasnim A. <tasu2301@gmail.com>
2024-10-06 23:05:08 +0600

    Pandas

---
+++ Day-25/main.py
@@ -0,0 +1,14 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title('District Games')
+image = 'bangla21.gif'
+screen.addshape(image)
+screen.screensize(canvwidth=713, canvheight=971)
+
+image = 'bangla21.gif'
+screen.addshape(image)
+
+
+turtle.shape(image)
+screen.exitonclick()
