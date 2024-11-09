44748cbc05a56cc821d4f050cf4f2343caff76e2
Tasnim A. <tasu2301@gmail.com>
2024-11-09 11:13:38 +0600

    Working with csv file, Pandas

--- Day-25/main.py
+++ Day-25/main.py
@@ -2,12 +2,13 @@ import turtle
 
 screen = turtle.Screen()
 screen.title('District Games')
-image = 'bangla21.gif'
-screen.addshape(image)
-screen.screensize(canvwidth=713, canvheight=971)
 
-image = 'bangla21.gif'
+# image = 'E:/GitHub/100DaysOfPython/Day-25/gifs/gifgit.gif'
+image = 'E:/GitHub/100DaysOfPython/Day-25/gifs/us_states_img.gif'
 screen.addshape(image)
+
+# screen.screensize(canvwidth=346, canvheight=470)
+screen.screensize(canvwidth=725, canvheight=491)
 
 
 turtle.shape(image)
--- Day-25/squirrel_data.py
+++ Day-25/squirrel_data.py
@@ -3,13 +3,10 @@ import pandas as pd
 df = pd.read_csv('Squirrel_Census.csv')
 
 color = df.groupby(['Primary Fur Color']).count()
-# print(color)
-# print(type(color)) # dataframe
 
 color_series = color['X']
-# print(color_series)
-# print(type(color_series)) # Series
 
 new_df = pd.DataFrame({'count': color_series})
 print(new_df)
+
 new_df.to_csv('numberOfSquirell.csv')
