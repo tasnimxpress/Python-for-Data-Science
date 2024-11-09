065b34c5bec71cfa9d94c7a5eb2b62af0670afc8
Tasnim A. <tasu2301@gmail.com>
2024-11-09 14:45:25 +0600

    Working with csv files, pandas

--- Day-25/main.py
+++ Day-25/main.py
@@ -1,15 +1,52 @@
 import turtle
+import pandas as pd
 
 screen = turtle.Screen()
-screen.title('District Games')
+screen.title('Division Games')
 
-# image = 'E:/GitHub/100DaysOfPython/Day-25/gifs/gifgit.gif'
-image = 'E:/GitHub/100DaysOfPython/Day-25/gifs/us_states_img.gif'
+image = 'E:/GitHub/100DaysOfPython/Day-25/gifs/ezgif.gif'
 screen.addshape(image)
-
-# screen.screensize(canvwidth=346, canvheight=470)
-screen.screensize(canvwidth=725, canvheight=491)
-
-
 turtle.shape(image)
-screen.exitonclick()
+
+# get corrdinate
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+df = pd.read_csv("E:/GitHub/100DaysOfPython/Day-25/division.csv")
+
+
+def division(answer):
+    corrdinate = df[df['division'] == answer]
+    X = corrdinate['x'].iloc[0]
+    Y = corrdinate['y'].iloc[0]
+    return (X, Y)
+
+
+score = 0
+length = len(df)
+all_answer = []
+t = turtle.Turtle()
+t.hideturtle()
+t.penup()
+
+while score != length:
+    answer = screen.textinput(title=f"{score}/{length} Guess the Division",
+                              prompt="Enter your guess?").capitalize()
+
+    if answer == 'Exit':
+        missing_div = []
+        for division in df['division'].values:
+            if division not in all_answer:
+                missing_div.append(division)
+        missing_data = pd.DataFrame(missing_div)
+        missing_data.to_csv(
+            'E:/GitHub/100DaysOfPython/Day-25/missing_division.csv')
+        break
+    elif answer in df['division'].values and answer not in all_answer:
+        position = division(answer)
+        t.goto(position)
+        all_answer.append(answer)
+        score += 1
+        t.write(answer)
